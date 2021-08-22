79f1c2e973b381d62ed19972252cf9acd143f774
Saiful Islam Adar <saifulislamadar@gmail.com>
2021-08-22 22:12:20 +0600

    added generating smart replies

--- android/src/main/java/com/reactnativesmartreply/SmartReplyModule.java
+++ android/src/main/java/com/reactnativesmartreply/SmartReplyModule.java
@@ -6,7 +6,19 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.nl.smartreply.SmartReply;
+import com.google.mlkit.nl.smartreply.SmartReplyGenerator;
+import com.google.mlkit.nl.smartreply.SmartReplySuggestion;
+import com.google.mlkit.nl.smartreply.SmartReplySuggestionResult;
+import com.google.mlkit.nl.smartreply.TextMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ReactModule(name = SmartReplyModule.NAME)
 public class SmartReplyModule extends ReactContextBaseJavaModule {
@@ -28,6 +40,51 @@ public class SmartReplyModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void multiply(int a, int b, Promise promise) {
         promise.resolve(a * b);
+    }
+
+    @ReactMethod
+    public void generateSmartReply(ReadableArray messages, Promise promise) {
+      List<TextMessage> conversation = new ArrayList<TextMessage>();
+      for (int i = 0 ; i < messages.size(); i++){
+        ReadableMap conversationObject = messages.getMap(i);
+        boolean isLocalUser = conversationObject.getBoolean("isLocalUser");
+        String message = conversationObject.getString("message");
+        String userId = conversationObject.getString("userId");
+        if(!isLocalUser){
+          conversation.add(TextMessage.createForRemoteUser(
+            message, System.currentTimeMillis(), userId));
+        }else {
+          conversation.add(TextMessage.createForLocalUser(
+            message, System.currentTimeMillis()));
+        }
+      }
+      SmartReplyGenerator smartReply = SmartReply.getClient();
+      smartReply.suggestReplies(conversation).addOnSuccessListener(new OnSuccessListener<SmartReplySuggestionResult>() {
+        @Override
+        public void onSuccess(@NonNull SmartReplySuggestionResult smartReplySuggestionResult) {
+          ArrayList<String> resultTexts = new ArrayList<String>();
+          if (smartReplySuggestionResult.getStatus() == SmartReplySuggestionResult.STATUS_NOT_SUPPORTED_LANGUAGE) {
+            // The conversation's language isn't supported, so
+            // the result doesn't contain any suggestions.
+          } else if (smartReplySuggestionResult.getStatus() == SmartReplySuggestionResult.STATUS_SUCCESS) {
+            // Task completed successfully
+            // ...
+
+            for (SmartReplySuggestion suggestion : smartReplySuggestionResult.getSuggestions()) {
+              String replyText = suggestion.getText();
+              resultTexts.add(replyText);
+            }
+          }
+          promise.resolve(resultTexts);
+        }
+      }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+          // Task failed with an exception
+          // ...
+          promise.reject(e);
+        }
+      });;
     }
 
     public static native int nativeMultiply(int a, int b);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import { NativeModules } from 'react-native';
 
-type SmartReplyType = {
-  multiply(a: number, b: number): Promise<number>;
+// type SmartReplyType = {
+//   multiply(a: number, b: number): Promise<number>;
+// };
+
+type SmartConvReplyType = {
+  generateSmartReply(messages: Array<Object>): Promise<Array<String>>;
 };
 
 const { SmartReply } = NativeModules;
 
-export default SmartReply as SmartReplyType;
+export default SmartReply as SmartConvReplyType;
